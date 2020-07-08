f224a322168a94c3d96a9aba2fc0e7f2208e15e1
Umer Kamran <ukamran@uwaterloo.ca>
2020-07-08 17:38:49 -0400

    Defined preliminary classes for basic shapes

---
+++ code/genetic-algorithm.py
@@ -0,0 +1,88 @@
+import time
+import random
+
+import numpy as np
+
+from scipy.special import comb
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as ptc
+
+from shapely.geometry import Point as SPoint
+from shapely.geometry import Polygon as SPolygon
+from shapely.geometry import LineString as SLine
+
+
+class Point(SPoint):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.center = (self.x, self.y)
+
+    def scale(self, sx, sy):
+        return Point(self.x * sx, self.y * sy)
+
+    def rotate(self, theta):
+        return Point(
+            self.x * np.cos(theta) - self.y * np.sin(theta),
+            self.x * np.sin(theta) + self.y * np.cos(theta)
+        )
+
+    def translate(self, point):
+        return Point(self.x + point.x, self.y + point.y)
+
+
+class Line(SLine):
+    def __init__(self, first, final):
+        super().__init__([first.center, final.center])
+        self.first = first
+        self.final = final
+        self.dx = final.x - first.x
+        self.dy = final.y - first.y
+        self.theta = np.arctan2(self.dy, self.dx)
+
+    def divide(self, d):
+        ix = self.dx / d
+        iy = self.dy / d
+        segments = []
+        previous = self.first
+        for i in range(1, d):
+            current = Point(self.first.x+ix*i, self.first.y+iy*i)
+            segments.append(Line(previous, current))
+            previous = current
+        segments.append(Line(previous, self.final))
+        return segments
+
+
+class Polygon(SPolygon):
+    def __init__(self, sx, sy, theta, t):
+        corners = [
+            Point(0.0, 0.0).scale(sx, sy).rotate(theta).translate(t).center,
+            Point(1.0, 0.0).scale(sx, sy).rotate(theta).translate(t).center,
+            Point(1.0, 1.0).scale(sx, sy).rotate(theta).translate(t).center,
+            Point(0.0, 1.0).scale(sx, sy).rotate(theta).translate(t).center
+        ]
+        super().__init__(corners)
+        self.datum = corners[0]
+        self.width = sx
+        self.height = sy
+        self.rotation = theta
+
+    def angle(self):
+        # convert from radians to degrees
+        return self.rotation * 180.0 / np.pi
+
+
+def main():
+    # ------- temporary: to be updated shortly -------
+
+    example_line = Line(Point(1, 1), Point(11, 11))
+    print(example_line.dx, example_line.dy)
+
+    example_poly = Polygon(5, 5, 0, Point(5, 5))
+    print(example_poly.width, example_poly.height)
+
+    # input("Press Enter to Exit")
+
+
+if __name__ == "__main__":
+    main()
