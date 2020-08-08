d7d74221371378c03f84aa7e17dc7a75d665723f
Umer Kamran <ukamran@uwaterloo.ca>
2020-08-08 13:26:42 -0400

    Moved the visualization code to a separate file

--- code/genetic-algorithm.py
+++ code/genetic-algorithm.py
@@ -5,12 +5,10 @@ import numpy as np
 
 from scipy.special import comb
 
-import matplotlib.pyplot as plt
-import matplotlib.patches as ptc
-
-import common.environment as env
+import common.environment as environment
 import common.inputs as inputs
 
+from common.visualize import visualizeResult, scatterPlot
 from common.geometry import Point, Line
 
 
@@ -124,75 +122,6 @@ def select(graded, evolved, count):
     return graded
 
 
-def visualize(grid, boundaries, obstacles, title, population=None, optimal=None):
-    fig, ax = plt.subplots()
-
-    ax.set_title(title, weight='bold')
-
-    ax.annotate(
-        "FIRST", (grid.first.x, grid.minimum.y - 0.1),
-        horizontalalignment='center',
-        verticalalignment='top',
-        weight='bold', color='w',
-    )
-
-    ax.annotate(
-        "FINAL", (grid.final.x, grid.maximum.y + 0.0),
-        horizontalalignment='center',
-        verticalalignment='bottom',
-        weight='bold', color='w'
-    )
-
-    for obstacle in obstacles:
-        rectangle = ptc.Rectangle(
-            obstacle.datum,
-            obstacle.width,
-            obstacle.height,
-            obstacle.angle(),
-            edgecolor='None', facecolor='grey', alpha=1.0
-        )
-        ax.add_patch(rectangle)
-
-    for boundary in boundaries:
-        rectangle = ptc.Rectangle(
-            boundary.datum,
-            boundary.width,
-            boundary.height,
-            boundary.angle(),
-            edgecolor='None', facecolor='black', alpha=1.0
-        )
-        ax.add_patch(rectangle)
-
-    if population is not None:
-        for path in population:
-            px = [point.x for point in path.points]
-            py = [point.y for point in path.points]
-            ax.plot(px, py, 'y-', alpha=0.2, markersize=4)
-
-    if optimal is not None:
-        px = [point.x for point in optimal.points]
-        py = [point.y for point in optimal.points]
-        ax.plot(px, py, 'c-', alpha=0.8, markersize=4)
-
-    ax.plot(grid.first.x, grid.first.y, 'co')
-    ax.plot(grid.final.x, grid.final.y, 'mo')
-
-    ax.grid()
-
-    plt.axis('scaled')
-    plt.show()
-
-
-def scatterPlot(x, y, title, xlabel, ylabel):
-    fig, ax = plt.subplots()
-    ax.set_title(title)
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
-    plt.scatter(x, y, marker='o', c='c')
-    plt.grid()
-    plt.show()
-
-
 def main(args=None):
 
     filename = "genetic-algorithm"
@@ -202,9 +131,9 @@ def main(args=None):
 
     arguments = inputs.parse(filename, args)
 
-    grid, boundaries, obstacles = env.generate(arguments)
+    grid, boundaries, obstacles = environment.generate(arguments)
 
-    visualize(grid, boundaries, obstacles, "Environment")
+    visualizeResult(grid, boundaries, obstacles, "Environment")
 
     startTime = time.time()
 
@@ -261,11 +190,11 @@ def main(args=None):
 
     print("Time Elapsed:", endTime - startTime)
 
-    visualize(grid, boundaries, obstacles, "Initial Population", initialPopulation)
+    visualizeResult(grid, boundaries, obstacles, "Initial Population", initialPopulation)
 
-    visualize(grid, boundaries, obstacles, "Final Population", finalPopulation)
+    visualizeResult(grid, boundaries, obstacles, "Final Population", finalPopulation)
 
-    visualize(grid, boundaries, obstacles, "Optimal Path", None, optimalPath)
+    visualizeResult(grid, boundaries, obstacles, "Optimal Path", None, optimalPath)
 
     scatterPlot(
         np.arange(1, arguments["evolutionMax"] + 1), averageFitness,
